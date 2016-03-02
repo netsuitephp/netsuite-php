7e95b6cdced9dd7e2fc98c914aa1b16bae524c2e
ryanwinchester <fungku@gmail.com>
2016-03-02 14:45:47 -0800

    extract logging to separate class

--- src/NetSuiteClient.php
+++ src/NetSuiteClient.php
@@ -54,18 +54,9 @@ class NetSuiteClient
 
         $response = $this->client->__soapCall($operation, array($parameter), null, $this->soapHeaders);
 
-        $this->logSoapCall($this->client, $operation);
+        $this->logSoapCall($operation);
 
         return $response;
-    }
-
-    /**
-     * SoapClient, even with keep-alive set to false, keeps sending the JSESSIONID cookie back to
-     * the server on subsequent requests. Un-setting the cookie will prevent this.
-     */
-    private function fixWtfCookieBug()
-    {
-        $this->client->__setCookie("JSESSIONID");
     }
 
     /**
@@ -184,6 +175,15 @@ class NetSuiteClient
     }
 
     /**
+     * SoapClient, even with keep-alive set to false, keeps sending the JSESSIONID cookie back to
+     * the server on subsequent requests. Un-setting the cookie will prevent this.
+     */
+    private function fixWtfCookieBug()
+    {
+        $this->client->__setCookie("JSESSIONID");
+    }
+
+    /**
      * Turn request logging on or off.
      *
      * @param bool $on
@@ -204,60 +204,15 @@ class NetSuiteClient
     }
 
     /**
-     * Log the last soap call as request and response XML files.
+     * Log the last SOAP call.
      *
-     * @param SoapClient $client
      * @param string $operation
      */
-    private function logSoapCall(SoapClient $client, $operation)
+    private function logSoapCall($operation)
     {
         if ($this->config['logging'] && file_exists($this->config['log_path'])) {
-            $fileName = "fungku-netsuite-php-" . date("Ymd.His") . "-" . $operation;
-            $logFile = $this->config['log_path'] ."/". $fileName;
-
-            // REQUEST
-            $request = $logFile . "-request.xml";
-            $Handle = fopen($request, 'w');
-            $Data = $client->__getLastRequest();
-            $Data = cleanUpNamespaces($Data);
-
-            $xml = simplexml_load_string($Data, 'SimpleXMLElement', LIBXML_NOCDATA);
-
-            $privateFieldXpaths = array(
-                '//password',
-                '//password2',
-                '//currentPassword',
-                '//newPassword',
-                '//newPassword2',
-                '//ccNumber',
-                '//ccSecurityCode',
-                '//socialSecurityNumber',
-            );
-
-            $privateFields = $xml->xpath(implode(" | ", $privateFieldXpaths));
-
-            foreach ($privateFields as &$field) {
-                $field[0] = "[Content Removed for Security Reasons]";
-            }
-
-            $stringCustomFields = $xml->xpath("//customField[@xsitype='StringCustomFieldRef']");
-
-            foreach ($stringCustomFields as $field) {
-                $field->value = "[Content Removed for Security Reasons]";
-            }
-
-            $xml_string = str_replace('xsitype', 'xsi:type', $xml->asXML());
-
-            fwrite($Handle, $xml_string);
-            fclose($Handle);
-
-            // RESPONSE
-            $response = $logFile . "-response.xml";
-            $Handle = fopen($response, 'w');
-            $Data = $client->__getLastResponse();
-
-            fwrite($Handle, $Data);
-            fclose($Handle);
+            $logger = new Logger($this->config['log_path']);
+            $logger->logSoapCall($this->client, $operation);
         }
     }
 }
