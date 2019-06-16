e1437c1c6389529403839fc727af824fdb7e1a2b
F. Elias <adminsgroup@911myweb.com>
2019-06-16 15:00:35 -0400

    generated 2019_1 classes

---
+++ #git-sync.bat
@@ -0,0 +1,3 @@
+@echo off
+git pull upstream master
+timeout /t 10
--- src/NetSuiteService.php
+++ src/NetSuiteService.php
@@ -15,7 +15,7 @@
  * @link       http://www.netsuite.com/portal/developers/resources/suitetalk-sample-applications.shtml
  *
  * created:    2015-01-22  1:04 PM
- * updated:    2019-02-27 03:26:43 PM PST
+ * updated:    2019-06-16 06:51:57 PM UTC
  */
 
 namespace NetSuite;
@@ -24,7 +24,7 @@ use NetSuite\Classes;
 
 class NetSuiteService extends NetSuiteClient {
 
-    public $generated_from_endpoint = "2018_2";
+    public $generated_from_endpoint = "2019_1";
 
     /**
      * Service Call: login
